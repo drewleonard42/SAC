3b6cd417a358def8a495b3d14eb2b352c7675fee
Stuart Mumford <s.mumford@sheffield.ac.uk>
2013-07-18 10:20:13 +0100

    add SAC source from stuart's sac repo

---
+++ sac/src/vacusrpar.f
@@ -0,0 +1,17 @@
+!##############################################################################
+! include vacusrpar - gravity
+INTEGER,PARAMETER:: grav0_=neqpar+1
+INTEGER,PARAMETER:: grav1_=neqpar+1+1,grav2_=neqpar+2+1
+INTEGER,PARAMETER:: nu_=neqpar+4
+INTEGER,PARAMETER:: nspecialpar=2+2
+
+!CHARACTER*2 ,PARAMETER:: specialparname='nu'
+
+
+   CHARACTER*11,PARAMETER:: specialparname='grav1 grav2'
+
+
+
+
+! end include vacusrpar - gravity
+!##############################################################################
