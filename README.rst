4c270cab67f5eed11692641cb3cc35ee91dbd459
Stuart Mumford <stuart@mumford.me.uk>
2015-02-13 15:22:45 +0000

    Add the fabricate code for Python based building.

--- README.rst
+++ README.rst
@@ -1,1 +1,21 @@
-README
+The Sheffield Advanced Code
+===========================
+
+SAC is a 1-3D ideal MHD / HD solver that solves for pertubations on top of a 
+static background.
+
+Building SAC
+------------
+
+The standard way to build SAC is::
+
+    cd sac/src
+    make vac
+
+It is also possible to build SAC using Python + Fabricate::
+
+    cd sac/src
+    python sac_fabricate.py
+
+The fabricate method is designed to allow the automatic configuration of the 
+build parameters from other Python code. See https://github.com/SWAT-Sheffield/Flux-Surfaces.
---
+++ sac/src/sac_fabricate.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python
+from __future__ import print_function
+
+import sys
+sys.path.append("../../../")
+
+from fabricate import *
+
+try:
+    from scripts import sacconfig
+    cfg = sacconfig.SACConfig()
+except ImportError:
+    cfg = None
+
+if cfg:
+    F_compiler = cfg.compiler
+    F_flags = cfg.compiler_flags.split(' ')
+    options = cfg.vac_modules
+else:
+    print("No config module found using default values for mpi on intel")
+    F_compiler = 'mpif90'
+    F_flags = ['-free', '-mcmodel=medium']
+    options = ['vaccd', 'vacmpi']
+
+F_ext = '.f'
+pre_processor = './vacpp.sh'
+sources = ['vac', 'vacio', 'vacgrid', 'vacphys0', 'vacphys', 'vacusr']
+includes = ['vacpar', 'vacusrpar', 'vacdef']
+
+def build():
+    pre_process()
+    compile()
+    link()
+
+def pre_process():
+    for source in sources + includes + options:
+        run(pre_processor, source+'.t',  source+F_ext)
+
+def compile():
+    for source in sources + options:
+        run(F_compiler, *F_flags + ['-c', source+F_ext])
+
+def link():
+    objects = [s+'.o' for s in sources + options]
+    run(F_compiler, '-o', '../vac', objects)
+
+def clean():
+    autoclean()
+
+if __name__ == '__main__':
+    main()
