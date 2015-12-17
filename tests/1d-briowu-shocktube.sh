4f014dcc971738e14092b277bb3e2439ad324c34
Drew Leonard <andy.j.leonard@gmail.com>
2015-12-17 10:15:50 +0000

    Bash script to run brio-wu shock

---
+++ tests/1d-briowu-shocktube.sh
@@ -0,0 +1,21 @@
+set -e
+
+# Get to correct directory and configure VAC
+cd ~/stusac/sac/src/
+make cleanall
+./setvac -d=12 -phi=0 -z=0 -g=808 -p=mhd -u=nul -on=cd,mpi -off=tvd
+
+# Build
+make vac;
+
+# Change dirs again and run initiation script
+cd ../
+python inipar/briowu-test12.py
+
+# Run VAC
+#./vac < par/briowu-test12
+cp par/briowu-test12 vac.par
+mpirun -np 1 vac
+
+# Return to test directory
+cd tests/
